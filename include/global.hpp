70234c9d59a04d00b62b2e3a6b0ec2c8f2d04746
Happy-potamus <134818253+Happy-potamus@users.noreply.github.com>
2025-03-03 21:39:14 -0800

    lj

--- include/global.hpp
+++ include/global.hpp
@@ -19,7 +19,7 @@ namespace glb{
 
     #define P_IMU 20
     #define P_LDB_ROTATION 6
-    #define P_AUTOCLAMP 19
+    #define P_AUTOCLAMP 2
     #define P_COLORSORT 7
     #define P_X_POD 16
     #define P_Y_POD 14
--- src/main.cpp
+++ src/main.cpp
@@ -254,16 +254,38 @@ void autonomous() {
 	// }
 
 	//examples:
-	drive(1000, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
+	//drive(1000, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
 	//    dist, units,   timeout,  chainPos, speedlimit, autoclamp, pid type
 	// for timeout and speedlimit: std::nullopt is like not passing in anything do this to use default values (127 for speedlim, and taylor generated timeout)
 
-	turn(90, std::nullopt, 0, std::nullopt, M_TURN_MOGO);
+	//turn(90, std::nullopt, 0, std::nullopt, M_TURN_MOGO);
 	// angle, timeout, chainPos, speedlimit, pid type
 	// for timeout and speedlimit: std::nullopt is like not passing in anything do this to use default values (127 for speedlim, and taylor generated timeout)
 
 	//for pid types, if u dont pass in anything the function assumes you are using normal movements, no mogo
 	//use M_DRIVE_MOGO for driving with a mogo, M_TURN_MOGO for turning with a mogo
+	//intake.move(127);
+	drive(600, M_TICKS, std::nullopt, 0, std::nullopt, true);
+	turn(-90, std::nullopt, 0, std::nullopt);
+	drive(-800, M_TICKS, std::nullopt, 0, 70, true);
+	turn(20, std::nullopt, 0, std::nullopt, M_TURN_MOGO);
+	drive(700, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
+	turn(45, std::nullopt, 0, std::nullopt, M_TURN_MOGO);
+	drive(1100, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
+	turn(10, std::nullopt, 0, std::nullopt, M_TURN_MOGO);
+	drive(1200, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
+	lbPID==true;
+	//delay(00);
+	drive(-1200, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
+	turn(90, std::nullopt, 0, std::nullopt, M_TURN_MOGO);
+	drive(500, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
+	//wall stake here
+	drive(-500, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
+	turn(180, std::nullopt, 0, std::nullopt, M_TURN_MOGO);
+	drive(2300, M_TICKS, std::nullopt, 0, std::nullopt, true, M_DRIVE_MOGO);
+
+
+	
 
 
 
