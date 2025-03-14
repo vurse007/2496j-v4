a8e6e0005a87a922fccb82540b85a2995e4afed5
Happy-potamus <134818253+Happy-potamus@users.noreply.github.com>
2025-03-14 01:11:50 -0700

    autos done!!!

--- src/main.cpp
+++ src/main.cpp
@@ -340,7 +340,7 @@ void autonomous() {
 	pros::Task cs_eject_redTask(eject_red);
 	
 
-	//goal side
+	//RED goal side
 	// drive(380);
 	// turn(-63);
 	// lbPID=2;
@@ -379,6 +379,43 @@ void autonomous() {
 	// drive(2000);
 
 
+	//BLUE goal side
+	// drive(380);
+	// turn(63);
+	// lbPID=2;
+	// delay(700);
+	// drive(-1200, M_TICKS, std::nullopt, 0, 70, true, M_DRIVE_MOGO);
+	// lbPID=4;
+	// turn(-50, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	// drive(750, M_TICKS, std::nullopt, 0, 127, true, M_DRIVE_MOGO);
+	// doinker.set_value(true);
+	// delay(100);
+	// turn(-65, std::nullopt, 5, std::nullopt,M_TURN_MOGO);
+	// drive(75, M_TICKS, std::nullopt, 0, 127, true, M_DRIVE_MOGO);
+	// doinker2.set_value(true);
+	// delay(100);
+	// drive(-1300, M_TICKS, std::nullopt, 50, 127, true, M_DRIVE_MOGO);
+	// drive(-200, M_TICKS, std::nullopt, 0, 30, true, M_DRIVE_MOGO);
+	// doinker.set_value(false);
+	// doinker2.set_value(false);
+	// delay(400);
+	// turn(-50, std::nullopt, 5, std::nullopt,M_TURN_MOGO);
+	// eject_r=true;
+	// pros::Task cs_eject_redTask(eject_r);
+	// drive(500, M_TICKS, std::nullopt, 0, 127, true, M_DRIVE_MOGO);
+
+	// turn(-160, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	// drive(1600, M_TICKS, std::nullopt, 0, 127, true, M_DRIVE_MOGO);
+	// turn(110, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	// drive(1200, M_TICKS, std::nullopt, 50, 127, true, M_DRIVE_MOGO);
+	// drive(200, M_TICKS, 500, 0, 30, true, M_DRIVE_MOGO);
+	// delay(500);
+	// drive(-400, M_TICKS, 500, 0, 127, true, M_DRIVE_MOGO);
+	// turn(-45, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	
+	// mogo.set_value(false);
+	// lbPID=2;
+	// drive(2000);
 
 
 
@@ -387,7 +424,10 @@ void autonomous() {
 
 
 
-	//ring side
+
+
+
+	//RED ring side
 	// drive(400, M_TICKS, 1000, 0, 70, true, M_DRIVE_MOGO);
 	// turn(62);
 	// lbPID=2;
@@ -407,6 +447,36 @@ void autonomous() {
 	// turn(71, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
 	// drive(600, M_TICKS, std::nullopt, 0, 70, true, M_DRIVE_MOGO);
 	// turn(110, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	// drive(1400, M_TICKS, std::nullopt, 50, 127, true, M_DRIVE_MOGO);
+	// drive(500, M_TICKS, 800, 0, 60, true, M_DRIVE_MOGO);
+	// drive(-400, M_TICKS, std::nullopt, 50, 127, true,  M_DRIVE_MOGO);
+	// turn(0, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	
+	// drive(1700, M_TICKS, std::nullopt, 0, 127, true, M_DRIVE_MOGO);
+	// drive(700, M_TICKS, std::nullopt, 0, 127, true, M_DRIVE_MOGO);
+
+
+
+	//BLUE ring side
+	// drive(400, M_TICKS, 1000, 0, 70, true, M_DRIVE_MOGO);
+	// turn(-62);
+	// lbPID=2;
+	// delay(700);
+	// drive(-1400, M_TICKS, std::nullopt, 0, 70, true, M_DRIVE_MOGO);
+	// lbPID=4;
+	// turn(105, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	// eject_r=true;
+	// pros::Task cs_eject_redTask(eject_red);
+	// heading_correction_pid.kP = 0;
+	// heading_correction_pid.kI = 0;
+	// heading_correction_pid.kD = 0;
+	// arc_right(64, 700, 1000, 0, 127, M_ARC_MOGO);
+	// turn(180, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	// drive(600);
+	
+	// turn(-71, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
+	// drive(600, M_TICKS, std::nullopt, 0, 70, true, M_DRIVE_MOGO);
+	// turn(-110, std::nullopt, 0, std::nullopt,M_TURN_MOGO);
 	// drive(1400, M_TICKS, std::nullopt, 50, 127, true, M_DRIVE_MOGO);
 	// drive(500, M_TICKS, 800, 0, 60, true, M_DRIVE_MOGO);
 	// drive(-400, M_TICKS, std::nullopt, 50, 127, true,  M_DRIVE_MOGO);
